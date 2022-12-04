80668ffd20e772c13e64a81aa4934b209846a3bd
khoanguyen23 <khoahoang151@gmail.com>
2022-12-04 18:57:36 +0700

    upload createNFT

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// import { NFTStorage } from 'nft.storage'
+// import { NFTStorage, File, Blob } from 'nft.storage'
+// const NFT_STORAGE_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEQyMmZhREJkNjQ4MWFlZTBFMDlBMjVFNjNjYUFmRDhhMTI0Yjk0OTIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY3MDA2NTY2NzU2MywibmFtZSI6Imtob2Fob2FuZzUxMSJ9._L2m9_qhmLASwu10PMqAZxMaXP3ZJll-FV7Q_BqOYv0'
+// const client = new NFTStorage({ token: NFT_STORAGE_TOKEN })
+
 const IconService = window["icon-sdk-js"].default;
 const httpProvider = new IconService.HttpProvider(
   "https://sejong.net.solidwallet.io/api/v3"
@@ -12,16 +17,19 @@ var avatar; // Ảnh đại diện (String)
 var allPublicNFTs; // Tất cả ảnh NFT public (kiểu mảng chuôi ["anh1.png", "anh2.png"] )
 const listItems = [];
 const marketplace = document.querySelector("#content1");
+
 function showAll() {
+  getPublicNFTs();
   allPublicNFTs.map((publicNFT) => {
     const img = document.createElement("img");
+ 
     img.src = publicNFT;
     console.log(img);
     const div = document.createElement("div");
     div.setAttribute("class", "marketplace-card");
     listItems.push(div);
     div.innerHTML = `
-        <img class="marketplace-card__img" src= "${img}" >
+        <img class="marketplace-card__img" src= "${img.src}" >
         <div class="marketplace-card__title">
         <div class="marketplace-card__title-info">
           <p class="marketplace-card__title-name">Crazy Apes</p>
@@ -247,7 +255,6 @@ function getAvatar() {
 }
 
 function getPublicNFTs() {
-    
   const getNFTsEvent = new CustomEvent('ICONEX_RELAY_REQUEST', {
       detail: {
           type: 'REQUEST_JSON-RPC',
@@ -267,14 +274,11 @@ function getPublicNFTs() {
           }
       }
   });
-  
   window.dispatchEvent(getNFTsEvent);
   const eventHandler = async event => {
       window.removeEventListener('ICONEX_RELAY_RESPONSE', eventHandler);
       const { type, payload } = event.detail;
       if (type === 'RESPONSE_JSON-RPC') {
-      
-
           console.log(payload);
           allPublicNFTs = payload.result;
           }
@@ -355,3 +359,224 @@ function showImage(imageList) {
   });
 }
 showImage(imageList);
+
+const swiperCardFlex = new Swiper('.card-flex', {
+  breakpoints: {
+      992: {
+          slidesPerView: 3,
+      },
+      576: {
+          slidesPerView: 2.1,
+      },
+      0: {
+          slidesPerView: 1.1,
+      }
+  },
+
+  navigation: {
+      nextEl: '.card__explore'
+  }
+});
+
+const swiperUserFlex = new Swiper('.users__card-flex', {
+  breakpoints: {
+      992: {
+          slidesPerView: 3,
+      },
+      576: {
+          slidesPerView: 2.1,
+      },
+      0: {
+          slidesPerView: 1.1,
+      }
+  },
+
+  navigation: {
+      nextEl: '.user__explore'
+  }
+});
+
+const swiperSellers = new Swiper('.top-sellers__swiper', {
+  breakpoints: {
+      992: {
+          slidesPerView: 3,
+      },
+      768: {
+          slidesPerView: 2,
+      },
+      0: {
+          slidesPerView: 1,
+      }
+  },
+
+  navigation: {
+      nextEl: '.top-sellers__explore'
+  }
+});
+
+let checkbox = document.getElementById('menu-toggle')
+
+checkbox.onclick = () => {
+  if (checkbox.checked) {
+      document.body.style.overflow = 'hidden';
+  } else{
+      document.body.style.overflow = 'visible';
+  }
+}
+
+function footerOpen(count) {
+  switch(count){
+      case 1:
+          if(document.querySelector('.market-place').classList[2]){
+              document.querySelector('.market-place').classList.remove('open')
+          } else{
+              document.querySelector('.market-place').classList.add('open')
+          }
+          break
+      case 2:
+          if(document.querySelector('.collectibles').classList[2]){
+              document.querySelector('.collectibles').classList.remove('open')
+          } else{
+              document.querySelector('.collectibles').classList.add('open')
+          }
+          break
+      case 3:
+          if(document.querySelector('.support').classList[2]){
+              document.querySelector('.support').classList.remove('open')
+          } else{
+              document.querySelector('.support').classList.add('open')
+          }
+          break
+      case 4:
+          if(document.querySelector('.legal').classList[2]){
+              document.querySelector('.legal').classList.remove('open')
+          } else{
+              document.querySelector('.legal').classList.add('open')
+          }
+          break
+      case 5:
+          if(document.querySelector('.follow').classList[2]){
+              document.querySelector('.follow').classList.remove('open')
+          } else{
+              document.querySelector('.follow').classList.add('open')
+          }
+          break
+  }
+
+}
+
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 0) {document.querySelector('header').classList.add('scrolled')}
+  else {document.querySelector('header').classList.remove('scrolled')}
+});
+
+setInterval(() => counters(), 1000);
+
+function counters(){
+  let hour
+  let minutes
+  let second
+
+  let countersArr = document.querySelectorAll('.seller-card__counter')
+
+  for (let counter of countersArr) {
+      second = counter.children[2].firstElementChild.innerHTML
+      minutes = counter.children[1].firstElementChild.innerHTML
+      hour = counter.children[0].firstElementChild.innerHTML
+
+      if (second - 1 >= 0) {
+          counter.children[2].firstElementChild.innerHTML = second - 1
+      } else if (minutes - 1 >= 0) {
+          counter.children[2].firstElementChild.innerHTML = 60
+          counter.children[1].firstElementChild.innerHTML = minutes - 1
+      } else {
+          counter.children[2].firstElementChild.innerHTML = 60
+          counter.children[1].firstElementChild.innerHTML = 60
+          counter.children[0].firstElementChild.innerHTML = hours - 1
+      }
+  }
+}
+
+function collectionBrandActive() {
+  document.querySelector('.grid__brand').classList.add('active')
+  document.querySelector('.grid__art').classList.remove('active')
+
+  document.querySelector('.collection__brand').classList.add('active')
+  document.querySelector('.collection__art').classList.remove('active')
+}
+
+function collectionArtActive() {
+  document.querySelector('.grid__art').classList.add('active')
+  document.querySelector('.grid__brand').classList.remove('active')
+
+  document.querySelector('.collection__art').classList.add('active')
+  document.querySelector('.collection__brand').classList.remove('active')
+}
+
+function ButtonActive(buttonNum) {
+  let gridButton = document.querySelector('.collection__grid')
+
+  for (let i = 0; i < 8; i++) {
+      if (gridButton.children[i].firstElementChild.classList[2] = 'active') {
+          gridButton.children[i].firstElementChild.classList.remove('active')
+          document.querySelector('.collection__img').children[i].classList.remove('active')
+      }
+  }
+
+  gridButton.children[buttonNum].firstElementChild.classList.add('active')
+  document.querySelector('.collection__img').children[buttonNum].classList.add('active')
+
+  document.querySelector('collection__img')
+}
+
+const imgs = document.getElementById("imgs");
+const img = document.querySelectorAll("#imgs img");
+
+let idx = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+idx++;
+changeImage();
+}
+
+function changeImage() {
+if (idx > img.length - 1) {
+ idx = 0;
+} else if (idx < 0) {
+ idx = img.length - 1;
+}
+
+imgs.style.transform = `translateX(${-idx * 480}px)`;
+}
+
+function resetInterval() {
+clearInterval(interval);
+interval = setInterval(run, 2000);
+}
+
+const inputImg = document.querySelector('#input-img')
+
+inputImg.addEventListener('change', (e) => {
+ let file = e.target.files[0]
+
+ if (!file) return
+
+ let img = document.createElement('img')
+ img.src = URL.createObjectURL(file)
+
+ document.querySelector('.preview').appendChild(img)
+})
+const inputAvatar = document.querySelector('#input-avatar')
+
+inputAvatar.addEventListener('change', (e) => {
+ let file = e.target.files[0]
+
+ if (!file) return
+
+ let img = document.createElement('img')
+ img.src = URL.createObjectURL(file)
+
+ document.querySelector('.preview1').appendChild(img)
+})
